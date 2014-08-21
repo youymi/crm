27de2847f28559bdf8904fc44a600ddefe141aa4
ghg <ghg@ecpisaas.com>
2014-08-21 17:17:51 +0800

    表结构调整

---
+++ src/main/java/com/epicsaas/app/crm/dao/gen/TodoContactorGeneratedMapper.java
@@ -0,0 +1,18 @@
+package com.epicsaas.app.crm.dao.gen;
+
+import com.epicsaas.app.crm.appobject.TodoContactorAO;
+import com.epicsaas.app.crm.entity.gen.TodoContactorCriteria;
+import com.epicsaas.common.dao.base.BaseGeneratedMapper;
+
+/**
+ * 自动生成的 TodoContactor 数据存取接口.
+ *
+ * <p>
+ * 该类于 2014-08-21 17:10:50 生成，请勿手工修改！
+ * </p>
+ *
+ * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
+ * @version 1.0.0.0, Aug 21, 2014
+ */
+public interface TodoContactorGeneratedMapper extends BaseGeneratedMapper<TodoContactorAO, TodoContactorCriteria> {
+}
