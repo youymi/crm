93a495d361d0d1345ab63c23ec47356ffa3ae713
youymi <youymi@163.com>
2014-09-01 10:01:40 +0800

    constant ..

--- src/main/java/com/epicsaas/app/crm/common/CrmConst.java
+++ src/main/java/com/epicsaas/app/crm/common/CrmConst.java
@@ -11,10 +11,19 @@ public class CrmConst {
 	 * 经理
 	 */
 	public static final String __CRM_MANAGER = "__CRM_MANAGER";
+	
+	public static final String __CRM_LEADER = "__CRM_LEADER";
+	
+	public static final String __CRM_NORMAL = "__CRM_NORMAL";
 
 	public static final String __APP_NAME = "客户关系管理";
 	
 	public static final String __APP_ID = "crm";
 	
 	public static final String __CRM_GROUP = "crmGroup";
+	
+	public static final String DD_TYPE_CUSTOMER_TYPE = "__customer_type";
+	
+	
+	
 }
