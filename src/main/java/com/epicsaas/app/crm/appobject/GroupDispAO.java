2c137774d324c5a70aff048d9bbcac1119d7aca8
youymi <youymi@163.com>
2014-09-01 14:33:17 +0800

    groupdispao

---
+++ src/main/java/com/epicsaas/app/crm/appobject/GroupDispAO.java
@@ -0,0 +1,29 @@
+package com.epicsaas.app.crm.appobject;
+
+import com.epicsaas.service.biz.userbase.dto.GroupDTO;
+
+public class GroupDispAO {
+
+ 
+	private GroupDTO  group;
+	private String names;
+
+	public String getNames() {
+		return names;
+	}
+
+	public void setNames(String names) {
+		this.names = names;
+	}
+
+	public GroupDTO getGroup() {
+		return group;
+	}
+
+	public void setGroup(GroupDTO g) {
+		this.group = g;
+	}
+	
+	
+
+}
