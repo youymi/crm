051b0b7394c5aacabe25ca5e1f69c518cb80b797
ghg <soa4java@163.com>
2014-09-01 11:23:01 +0800

    test

--- src/test/java/com/epicsaas/app/crm/DateTest.java
+++ src/test/java/com/epicsaas/app/crm/DateTest.java
@@ -1,5 +1,9 @@
+/*
+ * Copyright (c) 2012-2014, EpicSaaS Yuan Xin technology Co., Ltd.
+ * 
+ * All rights reserved.
+ */
 package com.epicsaas.app.crm;
-
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -12,38 +16,37 @@ import com.epicsaas.framework.util.DateTimeUtils;
 
 public class DateTest {
 
-	public List<String> getMonthList(int count, String endTime) {
-//		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
-//		SimpleDateFormat monthFormat = new SimpleDateFormat("yyyy-MM");
-		List<String> monthList = new ArrayList<String>();
+    public List<String> getMonthList(int count, String endTime) {
+        //		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        //		SimpleDateFormat monthFormat = new SimpleDateFormat("yyyy-MM");
+        List<String> monthList = new ArrayList<String>();
 
+        Date end = DateTimeUtils.parseStrToDate(endTime, DateTimeUtils.FORMAT_YMD);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(end);
+        calendar.add(Calendar.MONTH, 0 - count);
 
-		Date end = DateTimeUtils.parseStrToDate(endTime, DateTimeUtils.FORMAT_YMD);
-		Calendar calendar = Calendar.getInstance();  
-		calendar.setTime(end);  
-		calendar.add(Calendar.MONTH, 0-count);
-		
-		Date begin = calendar.getTime();
-		
-		int months = (end.getYear() - begin.getYear()) * 12+ (end.getMonth() - begin.getMonth());
-		
-		for (int i = 0; i <= months; i++) {
-		  calendar = Calendar.getInstance();  
-			calendar.setTime(begin);  
-			calendar.add(Calendar.MONTH, i);
-			String tmp =DateTimeUtils.formateDateToStr(calendar.getTime(), DateTimeUtils.FORMAT_YMD);
-			tmp = tmp.substring(0,tmp.lastIndexOf("-"));
-			monthList.add(tmp);
-		}
+        Date begin = calendar.getTime();
 
-		return monthList;
-	}
-	
-	public static void main(String[] args) {
-		DateTest test = new DateTest();
-		String end =	DateTimeUtils.formateDateToStr(new Date(), DateTimeUtils.FORMAT_YMD);
-		System.out.println(end);
-		List<String> list = test.getMonthList(7, end);
-		System.out.println(list.toString());
-	}
+        int months = (end.getYear() - begin.getYear()) * 12 + (end.getMonth() - begin.getMonth());
+
+        for (int i = 0; i <= months; i++) {
+            calendar = Calendar.getInstance();
+            calendar.setTime(begin);
+            calendar.add(Calendar.MONTH, i);
+            String tmp = DateTimeUtils.formateDateToStr(calendar.getTime(), DateTimeUtils.FORMAT_YMD);
+            tmp = tmp.substring(0, tmp.lastIndexOf("-"));
+            monthList.add(tmp);
+        }
+
+        return monthList;
+    }
+
+    public static void main(String[] args) {
+        DateTest test = new DateTest();
+        String end = DateTimeUtils.formateDateToStr(new Date(), DateTimeUtils.FORMAT_YMD);
+        System.out.println(end);
+        List<String> list = test.getMonthList(7, end);
+        System.out.println(list.toString());
+    }
 }
--- src/test/java/com/epicsaas/app/crm/Months.java
+++ src/test/java/com/epicsaas/app/crm/Months.java
@@ -1,18 +1,24 @@
+/*
+ * Copyright (c) 2012-2014, EpicSaaS Yuan Xin technology Co., Ltd.
+ * 
+ * All rights reserved.
+ */
 package com.epicsaas.app.crm;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Months {
-	public static void main(String[] args) {
-		Calendar c_begin = new GregorianCalendar();
-		Calendar c_end = new GregorianCalendar();
-		c_begin.set(2012, 5, 30);
-		c_end.set(2012, 11, 5);
-		while (c_begin.before(c_end)) {
-			int m = c_begin.get(Calendar.MONTH) + 1;
-			System.out.println(m);
-			c_begin.add(Calendar.MONTH, 1);
-		}
-	}
+
+    public static void main(String[] args) {
+        Calendar c_begin = new GregorianCalendar();
+        Calendar c_end = new GregorianCalendar();
+        c_begin.set(2012, 5, 30);
+        c_end.set(2012, 11, 5);
+        while (c_begin.before(c_end)) {
+            int m = c_begin.get(Calendar.MONTH) + 1;
+            System.out.println(m);
+            c_begin.add(Calendar.MONTH, 1);
+        }
+    }
 }
